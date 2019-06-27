6c69b65433c7c10bf48deabfe8239d6db7182b36
Sebastian Felix Schwarz <schwarz@nexus-ion.ai>
2019-06-27 16:12:21 +0200

    air data

--- handler.js
+++ handler.js
@@ -4,23 +4,23 @@ const s3 = new AWS.S3({region: 'eu-central-1'});
 
 
 const BUCKET = process.env.BUCKET;
-const USER = process.env.USER;
+const ROOMS = process.env.ROOMS;
 
 
 module.exports.air = async (event, context) => {
-  let user = USER.split(",");
-  let userdata = [];
-  for (let i=0;i<user.length;i++){
+  let room = ROOMS.split(",");
+  let roomdata = [];
+  for (let i=0;i<room.length;i++){
     let data = await s3.getObject({
       Bucket: BUCKET,
-      Key: user[i]+'.json'
+      Key: room[i]+'.json'
     }).promise();
-    userdata.push(JSON.parse(data.Body.toString()));
+    roomdata.push(JSON.parse(data.Body.toString()));
   }
 
   return {
     statusCode: 200,
-    body: userdata
+    body: roomdata
     //body: JSON.parse(data.Body.toString())
   };
 };
